efc86eec57acaba8edffe56dd8ee84f9eb483809
lsoliveiraa <129184876+lsoliveiraa@users.noreply.github.com>
2024-08-20 08:38:46 -0300

    dfgfdg

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 const teste = document.querySelector("teste");
+
+const titulo = document.querySelector(".teste2");
+
+titulo.textContent = "ola mundo";
+
+
+let contador = 1 ;
+
+let contador = [1,2,5,8,9];
+
+let perguntas = {
+
+        []
+        
+}
